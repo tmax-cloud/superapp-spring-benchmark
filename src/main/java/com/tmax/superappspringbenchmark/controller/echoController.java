c91bbfbd77e36c87f14bc6d7d3444aee26fccaf3
bongwoobak <66110096+bongwoobak@users.noreply.github.com>
2023-11-16 12:18:17 +0900

    add 0.3 sleep

--- src/main/java/com/tmax/superappspringbenchmark/controller/echoController.java
+++ src/main/java/com/tmax/superappspringbenchmark/controller/echoController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.*;
 public class echoController {
 
     @GetMapping
-    public String get(@RequestParam("msg") String msg){
+    public String get(@RequestParam("msg") String msg) throws InterruptedException{
         System.out.println("Incoming GET MSG : " + msg);
+        Thread.sleep(300);
         return msg;
     }
 
     @PostMapping
-    public String post(@RequestBody String msg){
+    public String post(@RequestBody String msg) throws InterruptedException{
         System.out.println("Incoming POST MSG : " + msg);
+        Thread.sleep(300);
         return msg;
     }
 }
