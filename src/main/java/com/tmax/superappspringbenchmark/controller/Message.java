3e6f60c49b2dc2b0b689059781e3296f41ebfb54
bongwoo <bongwoo_bak@tmax.co.kr>
2023-11-29 17:42:13 +0900

    fix post call to get  json body

---
+++ src/main/java/com/tmax/superappspringbenchmark/controller/Message.java
@@ -0,0 +1,14 @@
+package com.tmax.superappspringbenchmark.controller;
+
+public class Message {
+    private String msg;
+
+    // content 필드에 대한 getter와 setter
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String content) {
+        this.msg = content;
+    }
+}
--- src/main/java/com/tmax/superappspringbenchmark/controller/echoController.java
+++ src/main/java/com/tmax/superappspringbenchmark/controller/echoController.java
@@ -1,9 +1,15 @@
 package com.tmax.superappspringbenchmark.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.tmax.superappspringbenchmark.ThreadTracker;
+
+
 
 @RestController
 @RequestMapping("/echo")
@@ -25,11 +31,17 @@ public class echoController {
     }
 
     @PostMapping
-    public String post(@RequestBody String msg) throws InterruptedException {
+    public ResponseEntity<?> post(@RequestBody Message message) throws InterruptedException {
+        String msg = message.getMsg();
+        Map<String, Object> response = new HashMap<>();
+        response.put("msg", msg);
+        
         Thread.sleep(Integer.parseInt(env.getProperty("server.benchmark.sleep-time")));
         long threadId = Thread.currentThread().getId();
         threadTracker.addThreadId(threadId);
         System.out.println("Incoming POST MSG : " + msg + " on thread number: " + threadId);
-        return msg;
+        response.put("threadId", threadId);
+        System.out.println("How many threads did we use? : " + String.valueOf(threadTracker.getTotalThreadsUsed()));
+        return ResponseEntity.ok(response);
     }
 }
